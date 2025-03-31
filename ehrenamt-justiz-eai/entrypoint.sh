d1c59880f4e2b53a974a07a5d1c957372bc0ed84
udobrmuc <199229760+udobrmuc@users.noreply.github.com>
2025-03-31 15:23:40 +0200

    Dokumentation für docker-compose.yml und entrypoint.sh erweitert

--- ehrenamt-justiz-eai/entrypoint.sh
+++ ehrenamt-justiz-eai/entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+# Dieses Script wird im Container von Docker ausgeführt und hat folgende Aufgaben:
+#
+# - Zertiifikate mit Hilfe des curl-Befehls ermitteln und z.B. unter /tmp/.. speichern
+#     z.B. curl -o /tmp/LHM-....pem http://.../LHM-....pem
+#     ACHTUNG: Die notwendigen Zertifikate müssen vor dem Start dieses Scriptes manuell ergänzt werden!!
+#
+# - Importieren der Zertifikate mit Hilfe des keytool-Befehls
+#     z.B. keytool -import -noprompt -keystore /tmp/cacerts-lhm -file /tmp/LHM-....pem -storepass changeit -alias basename--LHM-....pem
+#     ACHTUNG: Die notwendigen Zertifikate müssen vor dem Start dieses Scriptes manuell ergänzt werden!!
+#
+# - Starten der EAI mit dem Profil "docker"
 
-# Die folgenden Zeilen sind notwendig, damit aus Docker heraus zugegriffen werden kann:
 echo "Zertifikate herunterladen"
 
 
 echo "Keytool: import von Zertifikaten"
 
-
 echo "start EAI"
-exec java -Dspring.profiles.active=docker -Djavax.net.ssl.trustStore=/tmp/cacerts-lhm -Djavax.net.ssl.trustStorePassword=changeit -jar /deployments/application.jar
+# ACHTUNG: Password hier ergänzen
+exec java -Dspring.profiles.active=docker -Djavax.net.ssl.trustStore=/tmp/cacerts-lhm -Djavax.net.ssl.trustStorePassword=.... -jar /deployments/application.jar
