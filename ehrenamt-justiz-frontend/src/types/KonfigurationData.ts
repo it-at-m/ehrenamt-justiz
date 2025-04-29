6ebdb1ea8ea35b392accca4f6d36fd51e1687b9b
udobrmuc <199229760+udobrmuc@users.noreply.github.com>
2025-04-29 10:05:50 +0200

    - Change to actual version of findbug - programs adapted according to current findbug version

---
+++ ehrenamt-justiz-frontend/src/types/KonfigurationData.ts
@@ -0,0 +1,12 @@
+export default interface KonfigurationData {
+  id: string | undefined;
+  ehrenamtjustizart: string;
+  bezeichnung: string;
+  aktiv: boolean;
+  amtsperiodevon: string;
+  amtsperiodebis: string;
+  altervon: number;
+  alterbis: number;
+  staatsangehoerigkeit: string;
+  wohnsitz: string;
+}
---
+++ ehrenamt-justiz-online/src/types/OnlineBewerbungData.ts
@@ -0,0 +1,8 @@
+export default interface OnlineBewerbungData {
+  vorname: string;
+  nachname: string;
+  geburtsdatum: string;
+  beruf: string;
+  telefonnummer: string;
+  mail: string;
+}
