bc2c256293c7eb691773a784dd1d358f61d9534b
udobrmuc <199229760+udobrmuc@users.noreply.github.com>
2025-05-12 10:21:02 +0200

    Online: router entfernt

--- ehrenamt-justiz-online/src/plugins/index.ts
+++ ehrenamt-justiz-online/src/plugins/index.ts
@@ -1,9 +1,8 @@
 import type { App } from "vue";
 
-import router from "../router";
 import pinia from "./pinia";
 import vuetify from "./vuetify";
 
 export function registerPlugins(app: App) {
-  app.use(vuetify).use(router).use(pinia);
+  app.use(vuetify).use(pinia);
 }
