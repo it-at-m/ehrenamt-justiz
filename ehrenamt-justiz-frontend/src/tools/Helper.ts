5a428656be4978fdd941dd5bb4401e9a0ebf5734
udobrmuc <199229760+udobrmuc@users.noreply.github.com>
2025-05-20 16:50:36 +0200

    Frontend: honorary justice in plain language

--- ehrenamt-justiz-frontend/src/tools/Helper.ts
+++ ehrenamt-justiz-frontend/src/tools/Helper.ts
@@ -1,6 +1,7 @@
 /**
  * return the type of honorary justice in formatted plain text
  * @param ehrenamtjustizart The type of honorary justice
+ * @return honorary justice in formatted plain text
  */
 export function formattedEhrenamtjustizart(
   ehrenamtjustizart: string | undefined
