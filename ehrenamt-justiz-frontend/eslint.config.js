5557ac272025a1d86f7f6639449c01142e40ed0a
udobrmuc <199229760+udobrmuc@users.noreply.github.com>
2025-06-23 10:48:13 +0200

    introduce i18n

--- ehrenamt-justiz-frontend/eslint.config.js
+++ ehrenamt-justiz-frontend/eslint.config.js
@@ -36,7 +36,12 @@ export default defineConfigWithVueTs(
       "@intlify/vue-i18n/no-duplicate-keys-in-locale": ["error"],
       "@intlify/vue-i18n/no-missing-keys-in-other-locales": ["error"],
       "@intlify/vue-i18n/no-unknown-locale": ["error"],
-      "@intlify/vue-i18n/no-unused-keys": ["error"],
+      "@intlify/vue-i18n/no-unused-keys": ["error",
+        {
+          "src": "./src",
+          "extensions": [".ts", ".vue"],
+          "ignores": ["general.schoeffen", "general.ehrenamtlicheVerwaltungsrichter"]
+        }],
     },
     settings: {
       "vue-i18n": {
