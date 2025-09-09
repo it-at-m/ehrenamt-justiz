d1e3fb8f59c6f50a507db7ea1def31272d8a638e
udobrmuc <199229760+udobrmuc@users.noreply.github.com>
2025-09-09 10:47:54 +0200

    Get URL of Gateway in Frontend from environment variable

--- ehrenamt-justiz-frontend/src/Constants.ts
+++ ehrenamt-justiz-frontend/src/Constants.ts
@@ -1,5 +1,6 @@
-export const API_BASE: string | undefined = import.meta.env
-  .VITE_VUE_APP_API_URL;
+// export const API_BASE: string | undefined = import.meta.env
+//  .VITE_VUE_APP_API_URL;
+export const API_BASE: string | undefined = import.meta.env.VITE_VUE_APP_GATEWAY_URL ? import.meta.env.VITE_VUE_APP_GATEWAY_URL : import.meta.env.VITE_VUE_APP_API_URL;
 export const ROUTES_HOME = "home";
 export const ROUTES_GETSTARTED = "getstarted";
 
