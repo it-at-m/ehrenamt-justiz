a6d168c9cfcfd3a436ac309d748d2580b7f468ff
udobrmuc <199229760+udobrmuc@users.noreply.github.com>
2026-08-06 08:26:58 +0200

    toggle light and dark mode and general improvements

---
+++ ehrenamt-justiz-frontend/src/components/common/ThemeToggleBtn.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn
+    :icon="theme.global.current.value.dark ? mdiWeatherSunny : mdiWeatherNight"
+    @click="toggleTheme"
+  />
+</template>
+
+<script setup lang="ts">
+import { mdiWeatherNight, mdiWeatherSunny } from "@mdi/js";
+import { useTheme } from "vuetify";
+import { VBtn } from "vuetify/components";
+
+const theme = useTheme();
+
+function toggleTheme(e: MouseEvent) {
+  theme.setTransitionOrigin(e.currentTarget as HTMLElement);
+  theme.toggle();
+}
+</script>
